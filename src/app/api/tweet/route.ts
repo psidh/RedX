df25fd612ec5180f8774dc6c18e05af24a6b9d19
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-06-11 16:45:01 +0530

    client/edge removed from  api/tweet

--- src/app/api/tweet/route.ts
+++ src/app/api/tweet/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse, NextRequest } from "next/server";
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export async function GET(req: NextRequest) {

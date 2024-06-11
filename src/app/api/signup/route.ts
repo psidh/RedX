160bc56d7d8e7a41995c4eba8d6b1557460745bb
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-06-11 16:46:07 +0530

    removed edge from api.signup

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth";
 const prisma = new PrismaClient();
 

7f202c49ef9a0b48bc20752785e0b45eceec4101
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-06-13 18:46:21 +0530

    middleware bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import  { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Toaster } from "react-hot-toast";
-const plus_sans = Inter({ subsets: ["latin"] });
+const plus_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "RedX",
